c6f76fcc46f11e033855db8fd5ae2104d9d5bc4e
patryk kupfer <kupferpatryk@gmail.com>
2025-12-27 17:15:41 +0100

    new-screen-for-add-meal

---
+++ app/add-new-meal.tsx
@@ -0,0 +1,28 @@
+import MealForm from '@/components/meal-form/MealForm'
+import AppContainer from '@/components/shared/AppContainer'
+import AppSafeView from '@/components/shared/AppSafeView'
+import ScreenHeader from '@/components/shared/ScreenHeader'
+import { addMeal } from '@/db/actions/meals/createMeal'
+import { useMealStore } from '@/stores/meals/useMealsStore'
+import { MealCreate } from '@/types/meal.type'
+import React from 'react'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+
+export default function AddNewMeal() {
+    const { addMeal: addMealToStore } = useMealStore()
+    const insets = useSafeAreaInsets();
+
+    function addMealToDb(data: MealCreate) {
+        const newMeal = addMeal(data);
+        addMealToStore(newMeal)
+    }
+
+    return (
+        <AppSafeView paddingBottom={insets.bottom} buttonsBackground>
+            <AppContainer>
+                <ScreenHeader title='Add meal' backHref={'/(tabs)/meals'} />
+                <MealForm onSubmit={addMealToDb} />
+            </AppContainer>
+        </AppSafeView>
+    )
+}
